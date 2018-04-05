a10ad05efcbac577213bc26110e8f3c2343f2092
glenio.nunes <glenio.nunes@LAB5-DCX-16.dcx.ufpb.br>
2018-04-05 11:50:58 -0300

    glenio passou aqui

--- src/Primeira.java
+++ src/Primeira.java
@@ -3,6 +3,8 @@ public class Primeira {
 	public static void main(String [] args){
 		System.out.println("AULA DE ENGENHARIA");
 		System.out.println(" gabriel leal ");
+		System.out.println(" glenio ");
+		
 	}
 
 }
